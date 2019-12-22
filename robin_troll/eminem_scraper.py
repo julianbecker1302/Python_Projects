b9d57f49cfefd6930f17163181f950b63d3231c2
Lukas <lvkas@ubuntu.speedport.ip>
2019-12-22 12:00:22 +0100

    Eminemscraper hinzugefügt

---
+++ robin_troll/eminem_scraper.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://de.wikipedia.org/wiki/Liste_der_Lieder_von_Eminem")
+soup = BeautifulSoup(response.text, "html.parser")
+
+lieder = []
+
+table = soup.find('table', {'class':'wikitable'})
+td = table.findNext("td")
+
+
+with open ("eminem_songs.txt", "w") as file:
+    for i in range(500):
+        tr = td.findNext("tr")
+        td = tr.findNext("td", text=True)
+        file.write(str(td)[4:-5])
--- robin_troll/silvester_song_script.py
+++ robin_troll/silvester_song_script.py
@@ -7,19 +7,21 @@ driver = webdriver.Chrome("/snap/bin/chromium.chromedriver")
 songlist=[]
 
 
-with open("ssiosongs.txt","r") as file:
+with open("eminem_songs.txt","r") as file:
     for line in file:
-        songlist.append(line[:-1] + ", SSIO")
+        songlist.append(line[:-1] + ", Eminem")
 
 
 
 for song in songlist:
-    driver.get('https://docs.google.com/forms/d/e/1FAIpQLScTDpHwL0spR0CGXMkEX3hXOnzuOf2B8yBvpyRAVEfTIryOAA/viewform')
-    textbox = driver.find_element_by_name("entry.1765775204")
-    textbox.send_keys(song)
-    sendebutton = driver.find_element_by_xpath("//div[@jsname='M2UYVd']")
-    sendebutton.click()
-
-
+    try:
+        if song != "":
+            driver.get('https://docs.google.com/forms/d/e/1FAIpQLScTDpHwL0spR0CGXMkEX3hXOnzuOf2B8yBvpyRAVEfTIryOAA/viewform')
+            textbox = driver.find_element_by_name("entry.1765775204")
+            textbox.send_keys(song)
+            sendebutton = driver.find_element_by_xpath("//div[@jsname='M2UYVd']")
+            sendebutton.click()
+    except:
+        print("no")
 
 driver.close()
