225d2b8d07ba7f4fa491aacb4cfab9f90e4555b6
lvckylvkas <1911034@stud.hs-mannheim.de>
2019-12-31 00:29:51 +0100

    removed error comments

--- ekr_script/ekrscript.py
+++ ekr_script/ekrscript.py
@@ -9,15 +9,13 @@ driver.get("https://www.ebay-kleinanzeigen.de/m-einloggen.html?targetUrl=/")
 
 login_email = driver.find_element_by_id("login-email")
 print("Insert your email")
-mail = input(">")  #error
+mail = input(">")
 login_pw = driver.find_element_by_id("login-password")
 print("Insert your password")
-pw = input(">")#error
+pw = input(">")
 
 login_email.send_keys(mail)
 login_pw.send_keys(pw)
 
 login_button = driver.find_element_by_id("login-submit")
 login_button.click()
-
-driver.close()
