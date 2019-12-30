0d1d8f02f2b2dcc8b12993b0bcacb88cd6fcd2f8
lvckylvkas <1911034@stud.hs-mannheim.de>
2019-12-31 00:24:39 +0100

    start of ekrscript

---
+++ ekr_script/ekrscript.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+import os
+import string
+
+
+driver = webdriver.Chrome("/snap/bin/chromium.chromedriver")    #chromedriver path
+
+driver.get("https://www.ebay-kleinanzeigen.de/m-einloggen.html?targetUrl=/")
+
+login_email = driver.find_element_by_id("login-email")
+print("Insert your email")
+mail = input(">")  #error
+login_pw = driver.find_element_by_id("login-password")
+print("Insert your password")
+pw = input(">")#error
+
+login_email.send_keys(mail)
+login_pw.send_keys(pw)
+
+login_button = driver.find_element_by_id("login-submit")
+login_button.click()
+
+driver.close()
